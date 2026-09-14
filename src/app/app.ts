4c04afd2455de2d1a1458c32a3274724e1ea8436
Anastasios Papamichalis <325844188+Anastasios-P@users.noreply.github.com>
2026-09-14 17:49:36 +0200

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { MainCalculator } from './main-calculator/main-calculator';
+import { PolynomialEquations } from './polynomial-equations/polynomial-equations';
 
 @Component({
   selector: 'app-root',
-  imports: [MainCalculator],
+  imports: [MainCalculator, PolynomialEquations],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
