4a014ba1cb2320c94b1104a2b46c4e74cfe36ce1
Anastasios Papamichalis <325844188+Anastasios-P@users.noreply.github.com>
2026-09-15 15:23:46 +0200

    Write the function for first degree polynomial equation

--- src/app/polynomial-equations/polynomial-equations.ts
+++ src/app/polynomial-equations/polynomial-equations.ts
@@ -15,6 +15,19 @@ export class PolynomialEquations {
   solution1: string = "";
 
   calculateEquation1(): void{
-    this.solution1 = "testing.. OK.";
+    // ax + b = 0, a = x1, b = x
+    // x = -b/a : a ≠ 0
+    let a = Number(this.x1);
+    let b = Number(this.x);
+
+    let solution = 0;
+
+    if(a == 0){
+      this.solution1 = "ERROR! The variable a can not be zero.";
+    }
+    else{
+      solution = -(b / a);
+      this.solution1 = "The solution of the equation " + String(a) + "x + " + String(b) + " = 0 is " + String(solution);
+    }
   }
 }
