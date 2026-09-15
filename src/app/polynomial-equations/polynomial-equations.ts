ac30ccb42f49332e1ba7b264e052b4bab0a71185
Anastasios Papamichalis <325844188+Anastasios-P@users.noreply.github.com>
2026-09-15 13:07:20 +0200

    Update polynomial-equations.ts

--- src/app/polynomial-equations/polynomial-equations.ts
+++ src/app/polynomial-equations/polynomial-equations.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-polynomial-equations',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './polynomial-equations.html',
   styleUrl: './polynomial-equations.css',
 })
-export class PolynomialEquations {}
+export class PolynomialEquations {
+  chosenPolynomialDegree: string = "";
+  x: string = "";
+  x1: string = "";
+
+  solution1: string = "";
+
+  calculateEquation1(): void{
+    this.solution1 = "testing.. OK.";
+  }
+}
