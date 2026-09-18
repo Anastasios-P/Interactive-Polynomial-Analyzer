e48d5b9ccf2e6b74177361facf4a847e685f488a
Anastasios Papamichalis <325844188+Anastasios-P@users.noreply.github.com>
2026-09-18 19:45:51 +0200

    Add a diagram
    just testing... Still working on it..

--- src/app/diagram-polynomial-equations/diagram-polynomial-equations.ts
+++ src/app/diagram-polynomial-equations/diagram-polynomial-equations.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
 @Component({
   selector: 'app-diagram-polynomial-equations',
@@ -6,4 +8,26 @@ import { Component } from '@angular/core';
   templateUrl: './diagram-polynomial-equations.html',
   styleUrl: './diagram-polynomial-equations.css',
 })
-export class DiagramPolynomialEquations {}
+export class DiagramPolynomialEquations {
+
+  xAxis: number[] = [1,2,3,4,5,6,7,8];
+  daten: number[] = [1,2,3,4,3,2,1,0];
+
+  public config: any = {
+  type: 'line',
+  data: {
+  labels: this.xAxis,
+  datasets: [{
+  label: '',
+  data: this.daten,
+  borderColor: 'rgb(75, 192, 192)',
+  }]
+  }
+  };
+  chart: any;
+
+  ngOnInit(): void{ 
+    var element = <HTMLCanvasElement>document.getElementById('equationDiagram');
+    this.chart = new Chart(element, this.config);  
+  }
+}
